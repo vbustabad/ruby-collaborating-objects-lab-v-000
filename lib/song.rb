2e16d383af089161dd683e540495ff8361e43b4c
Veronica Bustabad <vbustabad@gmail.com>
2018-04-05 01:48:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ class Song
   end
 
   def self.new_by_filename
-    new_instance = self.new(name)
-    new_instance = file_name.split("-")[1]
+    new_instance = Song.new(name)
+    new_instance.name = file_name.split("-")[1]
     new_instance.artist_name = file_name.split("-")[0]
     new_instance
   end
