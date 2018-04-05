979580a1bbe7143e3b531ccce9af072b6b3854bd
Veronica Bustabad <vbustabad@gmail.com>
2018-04-05 02:43:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,11 +11,11 @@ class Song
 
   def artist_name= (artist_name)
     self.artist = Artist.find_or_create_by_name(artist_name)
-    Artist.add_song(song)
   end
 
   def self.new_by_filename(file_name)
     new_song = file_name.split(" - ")[1]
+    new_song.artist_name = artist
     new_song
   end
 
