4661a2114bac56e8d320c40b38f06bf1e0dd0d7c
Veronica Bustabad <vbustabad@gmail.com>
2018-04-05 03:03:11 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,9 +13,7 @@ class MP3Importer
   end
 
   def import
-    self.files.each do |file|
-      Song.new_by_filename(file_name)
-    end
+    self.files.each do { |file| Song.new_by_filename(file_name) }
   end
 
 end
