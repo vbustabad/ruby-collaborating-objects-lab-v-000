2f88a71a7f2814f3fa8b4647eb656a8db3f4316f
Veronica Bustabad <vbustabad@gmail.com>
2018-04-05 03:25:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,10 +15,9 @@ class Song
   # end
 
   def self.new_by_filename(file_name)
-    binding.pry
-    new_song = Song.new(name)
-    new_song = file_name.split(" - ")
-    new_song.artist = Artist.find_or_create_by_name(artist_name)
+    song_array = file_name.split(" - ")
+    new_song = Song.new(song_array[1])
+    new_song.artist = Artist.find_or_create_by_name(song_array[0])
     new_song
   end
 
