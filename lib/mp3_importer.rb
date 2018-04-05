be447b32674c6fe98571ee57664274c68e43119f
Veronica Bustabad <vbustabad@gmail.com>
2018-04-05 03:39:03 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,19 +1,19 @@
 class MP3Importer
 
-  attr_accessor :path
+  attr_accessor :path, :file_array
 
   def initialize(path)
     @path = path
   end
 
   def files
-    Dir.glob("#{path}/*.mp3").collect do |file|
+    file_array = Dir.glob("#{path}/*.mp3").collect do |file|
       file_name = file.split("/")[4]
     end
   end
 
   def import
-    self.files.each do |file|
+    self.file_array.each do |file|
       Song.new_by_filename(file)
     end
   end
